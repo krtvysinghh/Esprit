f4e207dffd772519e506f38384054c914cfba547
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-07 22:27:43 +0530

    refactor(index): introduce production indexing engine

---
+++ crates/esprit-core-index/src/engine.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use std::path::Path;
+
+pub struct IndexEngine;
+
+impl IndexEngine {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn build(&self, _: impl AsRef<Path>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn update(&self, _: impl AsRef<Path>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn remove(&self, _: impl AsRef<Path>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn optimize(&self) -> Result<()> {
+        Ok(())
+    }
+}
