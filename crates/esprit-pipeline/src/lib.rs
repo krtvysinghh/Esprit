8950a77490055c73b1a1e9963b6438456419b073
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-07 22:56:51 +0530

    feat(core): pipeline utils testing framework

---
+++ crates/esprit-pipeline/src/lib.rs
@@ -0,0 +1,28 @@
+use anyhow::Result;
+
+pub trait Stage<T> {
+    fn run(&self, input: T) -> Result<T>;
+}
+
+pub struct Pipeline<T> {
+    stages: Vec<Box<dyn Stage<T>>>,
+}
+
+impl<T: 'static> Default for Pipeline<T> {
+    fn default() -> Self {
+        Self { stages: Vec::new() }
+    }
+}
+
+impl<T: 'static> Pipeline<T> {
+    pub fn add<S: Stage<T> + 'static>(&mut self, s: S) {
+        self.stages.push(Box::new(s));
+    }
+
+    pub fn execute(&self, mut value: T) -> Result<T> {
+        for s in &self.stages {
+            value = s.run(value)?;
+        }
+        Ok(value)
+    }
+}
---
+++ crates/esprit-utils/src/lib.rs
@@ -0,0 +1,7 @@
+use sha2::{Digest, Sha256};
+
+pub fn sha256(data: impl AsRef<[u8]>) -> String {
+    let mut h = Sha256::new();
+    h.update(data.as_ref());
+    hex::encode(h.finalize())
+}
