66d9ff2ca4faff77bbac6e5880dd5612be6ab78b
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-07 23:01:38 +0530

    refactor(app): introduce unified application crate

---
+++ crates/esprit-app/src/lib.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+
+pub struct App;
+
+impl Default for App {
+    fn default() -> Self {
+        Self
+    }
+}
+
+impl App {
+    pub fn boot() -> Result<Self> {
+        let _ = esprit_config::Config::load()?;
+        Ok(Self)
+    }
+}
